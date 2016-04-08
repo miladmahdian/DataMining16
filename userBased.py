199f348811ff10ad2fc7b51419d0cadf9e773e97
Akash Srivastava <srivastava.ak@husky.neu.edu>
2016-04-08 17:46:02 -0400

    updated with prediction

--- userBased.py
+++ userBased.py
@@ -75,7 +75,7 @@ def songCountMapping(userAttributes):
     
     return dict;
 
-#print("Creating User Attributes File");   
+print("Creating User Attributes File For Training Data");   
 UserAttributes = open('UserAttributes.txt','w');     
 
 with open("training.txt","r") as file:
@@ -93,14 +93,50 @@ with open("training.txt","r") as file:
     UserAttributes.write(prevUser + '\t' +userAttr+'\n');
     UserAttributes.close();
         
-print("Created User Attributes File");
+print("Created User Attributes File for Traing Data");
 
-similarityFile = open('similarityFile.txt','w');
+print("Creating User Attributes File For Testing Data");   
+UserAttributes = open('UserAttributesTesting.txt','w');     
+
+with open("testing.txt","r") as file:
+    userAttr='';
+    prevUser ='';
+    for line in file:
+        user,song,count=line.strip().split('\t');
+        if(prevUser == user or prevUser == ''):        
+            userAttr += song + ':' + count +',';
+        else:
+             UserAttributes.write(prevUser + '\t' +userAttr+'\n');
+             userAttr = song + ':' + count +',';
+             
+        prevUser = user;
+    UserAttributes.write(prevUser + '\t' +userAttr+'\n');
+    UserAttributes.close();
+        
+print("Created User Attributes File for Testing Data");
+
+print("Loading Testing Data in a Dictionary");   
+testUserAttr ={};
+
+with open("UserAttributesTesting.txt","r") as file:
+    for line in file:
+            user,attributes=line.strip().split('\t')
+            testUserAttr[int(user)] = attributes;
+    
+predictionFile = open('predictionFile.txt','w');
 
 print("Defining Similarity Between Users");
 with open("UserAttributes.txt","r") as file1:
     for user_i in file1:
          user1, user1Attr = user_i.strip().split('\t');
+         if int(user1) not in testUserAttr:
+             continue;
+         testingData = songCountMapping(testUserAttr[int(user1)]);
+         num ={};
+         den ={};
+         for key in testingData:
+            num[key] = -averageRating(user1Attr);
+            den[key] =0.0;
          with open("UserAttributes.txt","r") as file2:
              for user_j in file2:
                 user2, user2Attr = user_j.strip().split('\t');
@@ -111,7 +147,7 @@ with open("UserAttributes.txt","r") as file1:
                     user2_songcounts = songCountMapping(user2Attr)
                     Ruser1AvgCount = averageRating(user1Attr);
                     Ruser2AvgCount = averageRating(user2Attr);
-                    num =0.0;
+                    num1 =0.0;
                     user1den = 0.0;
                     user2den = 0.0;
                     for p in P:
@@ -119,21 +155,33 @@ with open("UserAttributes.txt","r") as file1:
                         Ruser2songcount = user2_songcounts[p]
                         devuser1 =  Ruser1songcount - Ruser1AvgCount
                         devuser2 =  Ruser2songcount - Ruser2AvgCount
-                        num += devuser1* devuser2;
+                        num1 += devuser1* devuser2;
                         user1den += math.pow(devuser1,2);
                         user2den += math.pow(devuser2,2);
-                    
                     
                     user1den = math.pow(user1den,0.5);
                     user2den = math.pow(user2den,0.5);
                     deno = user1den * user2den;
+                    if(deno !=0 and num1!=0):
+                        similarity = num1/deno;
+                        #similarityFile.write(user1 +','+user2+'\t'+str(similarity)+'\n');
+
+                    testingData = songCountMapping(testUserAttr[int(user1)]);  
+                    for key in testingData:
+                        if key in user2_songcounts:
+                            num[key] += similarity * (user2_songcounts[key] - Ruser2AvgCount)
+                            den[key] += similarity;
                     
-                    if(deno !=0 and num!=0):
-                        similarity = num/deno;
-                        similarityFile.write(user1 +','+user2+'\t'+str(similarity)+'\n');        
+             pred = 0;
+             testingData = songCountMapping(testUserAttr[int(user1)]);
+             for key in testingData: 
+                if(den[key] !=0 and num[key]!=0):                       
+                    pred = num[key]/den[key] + averageRating(user1Attr);
+                print 'Prediction: '+user1 +' , '+str(key)+'\t'+str(pred)+'\n';
+                predictionFile.write(user1 +' , '+str(key)+'\t'+str(pred)+'\n');
                 
                         
-similarityFile.close();
+predictionFile.close();
 print('Done');        
                 
                 
